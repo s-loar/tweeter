6167dceb878736bf3efb8baa2a5f962a8dee2938
Steven Loar <sjl7909@gmail.com>
2021-06-25 15:33:46 -0400

    Add scopes to Tweet model to hide deleted_at records by default and the options to include them, or just have them

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,11 @@
 class Tweet < ApplicationRecord
   validates :message, presence: true
 
+  default_scope { visible }
+  scope :visible, -> { where(deleted_at: nil) }
+  scope :with_deleted, -> { unscope(where: :deleted_at) }
+  scope :deleted, -> { with_deleted.where.not(deleted_at: nil) }
+
   after_create_commit { broadcast_prepend_to "tweets" }
   after_update_commit { broadcast_replace_to "tweets" }
   after_destroy_commit { broadcast_remove_to "tweets" }
